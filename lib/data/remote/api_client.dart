70c0e4003de25ba35da75371255acc9da910faeb
Lakmal Weerasekara <lakmal.weerasekara@nationstrust.com>
2022-02-19 00:38:54 +0530

    client doc

--- lib/data/remote/api_client.dart
+++ lib/data/remote/api_client.dart
@@ -6,7 +6,7 @@ import 'package:retrofit/retrofit.dart';
 part 'api_client.g.dart';
 
 @RestApi(baseUrl: '')
-
+// https://jsonplaceholder.typicode.com/
 abstract class ApiClient {
   factory ApiClient(Dio dio, {String baseUrl}) = _ApiClient;
 
